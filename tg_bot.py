6dffb094666b72407723847b8dd05f6d298fb961
Maria-kairos <mk4169317@gmail.com>
2024-11-20 09:58:20 +0300

    Add files via upload

---
+++ tg_bot.py
@@ -0,0 +1,59 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from aiogram.utils import executor
+from aiogram.dispatcher.filters.state import State
+from aiogram.dispatcher.filters.state import StatesGroup
+
+api_key = '8035950847:AAFtrprDtZnv7n_ihZWLQ4SuzVqMjQ7iTPk'
+bot = Bot(token=api_key)
+dsp = Dispatcher(bot=bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dsp.message_handler(text = 'Calories')
+async def set_age(message):
+    print('Началась команда "Calories"')
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dsp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+@dsp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+@dsp.message_handler(state = UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    weight = float(data['weight'])
+    growth = float(data['growth'])
+    age = float(data['age'])
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
+    await message.answer(f'Ваша норма калорий: {calories} ккал в сутки.')
+    await UserState.weight.set()
+    await state.finish()
+    print('Выведено сообщение: Ваша норма калорий: {calories} ккал в сутки.')
+
+@dsp.message_handler(commands=['start'])
+async def start(message):
+    print('Выведено сообщение: Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dsp.message_handler()
+async def all_messages(message):
+    print('Выведено сообщение: Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dsp, skip_updates=True)
